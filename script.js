eb0ee879c057b6cf69636036c320be53f1b69e13
ngritter <ngritter44@gmail.com>
2023-02-11 10:24:36 -0800

    Create grid

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('#container');
+
+function createGrid(row, column){
+    for (j=0; j<row; j++) {
+        const div = document.createElement('div');
+        div.classList.add("row");
+
+        container.appendChild(div);
+
+        function createColumn(count) {
+            for (i=0; i<count; i++) {
+                var divInternal = document.createElement('div');
+                divInternal.classList.add("block");
+                div.appendChild(divInternal);
+                }
+        }
+
+        createColumn(column);
+
+    }
+
+}
+
+createGrid(16,16);
